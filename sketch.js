cab50149ef443e0487e0185bfc789af8321a0c4a
ks7150 <144281932+ks7150@users.noreply.github.com>
2023-10-31 14:18:22 -0400

    HW02B

--- sketch.js
+++ sketch.js
@@ -1,6 +1,43 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+
+let song; 
+
+let peaks;
+
+function preload() {
+  song = loadSound("autumn-leaf_with-violins-74917.mp3");
 }
 
-function draw() {
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+peaks = song.getPeaks(width)
+
+
+
 }
+
+
+
+function draw() {
+  background(220, 20, 120);
+for(let i=0; i < peaks.length; i++){
+//line (i%width,height/2+peaks[i]*height/2, i%width, height/2)
+let x= i%width;
+noFill()
+strokeWeight(1/2)
+ellipse(x+peaks[x]*width/2, height/2+peaks[i]*height/2 , x, height/2)
+
+}
+}
+
+function mouseClicked() {
+  song.play();
+}
+
+
+
+
+
+
+
+
+
